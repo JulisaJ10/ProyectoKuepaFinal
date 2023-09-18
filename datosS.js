81606bd3b9141a0295b2911d649bb0fc6963988f
JulisaJ10 <55122258+JulisaJ10@users.noreply.github.com>
2023-09-17 22:21:30 -0600

    Archivos json

---
+++ datosS.js
@@ -0,0 +1,14 @@
+"sedes";[
+    {
+    "nombre del municipio": "Pilcaya",
+    "total de voluntarios": 15,
+    "total de personas": 500,
+
+    },
+    {
+        "nombre del municipio": "Tixtla",
+        "total de voluntarios": 10,
+        "total de personas": 2500,
+    
+    }
+]
---
+++ usuarios.js
@@ -0,0 +1,24 @@
+"usuarios";[{
+    "nombre": "Pedro",
+    "apellido": "Ramirez",
+    "sexo": "Masculino",
+    "edad": 22,
+    "correo": "pedro@example.com",
+    "contraseña": "clave789",
+    "area": ["Tallerista"],
+    "admin": false
+    },
+
+    
+    {
+
+    "nombre": "María",
+    "apellido": "Gómez",
+    "sexo": "Femenino",
+    "edad": 25,
+    "correo": "maria@example.com",
+    "contraseña": "contraseña456",
+    "area": ["Coordiandor"],
+    "admin": true
+    }
+]
